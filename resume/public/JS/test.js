c8580e116522eff821d833e22f02f47e122ae4bd
vegatch <stravinsky.anglade@mykenzie.snhu.edu>
2022-08-15 03:08:17 -0400

    adding forms to right hand side of the screen

---
+++ resume/public/JS/test.js
@@ -0,0 +1,13 @@
+let hideForm = () =>{
+    document.querySelector("#personForm").style.display = "none"
+    document.querySelector("#titleForm").style.display = "none"
+    document.querySelector("#contactForm").style.display = "none"
+    document.querySelector("#socioForm").style.display = "none"
+    document.querySelector("#resObjective").style.display = "none"
+
+    document.querySelector("#educationForm").style.display = "none"
+    document.querySelector("#techSkillsForm").style.display = "none"
+    document.querySelector("#marketSkillsForm").style.display = "none"
+    // document.querySelector("#projectsForm").style.display = "none"
+}
+hideForm()
