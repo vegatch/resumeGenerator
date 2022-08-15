509b157df6854365da2790d0ea63b975ead051e2
vegatch <stravinsky.anglade@mykenzie.snhu.edu>
2022-08-15 09:53:38 -0400

    make header sticky to the top

--- resume/public/JS/test.js
+++ resume/public/JS/test.js
@@ -8,6 +8,7 @@ let hideForm = () =>{
     document.querySelector("#educationForm").style.display = "none"
     document.querySelector("#techSkillsForm").style.display = "none"
     document.querySelector("#marketSkillsForm").style.display = "none"
-    // document.querySelector("#projectsForm").style.display = "none"
+    document.querySelector("#projectsForm").style.display = "none"
+    document.querySelector("#workForm").style.display = "none"
 }
 hideForm()
